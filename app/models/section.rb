e4d002f4272ad4c183ae5c21c5d2065e8657c675
Pcari <bensjae@gmail.com>
2018-07-25 01:04:45 -0700

    save

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,10 +1,11 @@
 class Section < ApplicationRecord
 
+
 	validates :name, presence: true, uniqueness: {case_sensitive: false}
 	has_many :food_items
 
 	def self.generate_defaults
-		["Breakfast", "Lunch", "Dinner", "HappyHour"].each do |name|
+		%w(Breakfast Lunch Dinner HappyHour).each do |name|
 			Section.find_or_create_by(name: name)
 		end
 	end
