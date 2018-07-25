4d4aa5aa3ad7f1d68f4b3f863a9db2b22748bdf9
Pcari <bensjae@gmail.com>
2018-07-25 00:10:19 -0700

    changed section.rb

--- app/models/section.rb
+++ app/models/section.rb
@@ -4,7 +4,7 @@ class Section < ApplicationRecord
 	has_many :food_items
 
 	def self.generate_defaults
-		%w(Breakfast Lunch Dinner HappyHour).each do |name|
+		["Breakfast", "Lunch", "Dinner", "HappyHour"].each do |name|
 			Section.find_or_create_by(name: name)
 		end
 	end
