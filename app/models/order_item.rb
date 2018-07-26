408531bcb03bfb790c035bac760bd3f5ed1fbde9
Jason Tsang <jasonrtsang@gmail.com>
2018-07-26 15:32:48 -0700

    Form constraints and visual updates

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,3 +1,5 @@
 class OrderItem < ApplicationRecord
-  belongs_to :food_item
+    validates :cost, :food_item, presence: true
+    validates :cost, :numericality => {:only_decimal => true}
+    belongs_to :food_item
 end
