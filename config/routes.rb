34f0e4ef80e0e5e7670c34fa9440cd2d2579d762
Jason Tsang <jasonrtsang@gmail.com>
2018-07-27 15:04:39 -0700

    Make index page Menu page

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   match ':controller(/:action(/:id))', :via => :get
 
- root 'welcome#index'
+ root 'welcome#menu'
 
 end
