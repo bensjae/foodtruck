91df6da5c4e1facdb8ba27ba94a49f8b41d054bd
seannam1218 <seannam1218@gmail.com>
2018-07-27 16:15:29 -0700

    triple join using weird method

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -5,7 +5,9 @@ class CustomersController < ApplicationController
   # GET /customers.json
   def index
     @customers = Customer.all
-    @customer_order = Customer.joins("INNER JOIN order_items, food_items ON customers.name = order_items.customer, food_items.id = order_items.food_item_id")
+   # @customer_order = Customer.joins("INNER JOIN order_items, food_items ON customers.name = order_items.customer, food_items.id = order_items.food_item_id")
+    @order_food = Order_Items.joins("INNER JOIN food_items ON food_items.id = order_items.food_item_id")
+    @customer_order = Customer.joins(:@order_food).where("customers.name = " + @order_food.customer + "")
 
   end
 
