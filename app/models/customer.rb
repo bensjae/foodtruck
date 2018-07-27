eb7fde79ebe834b39e728368e6c5b246ada30c67
Jason Tsang <jasonrtsang@gmail.com>
2018-07-26 17:05:34 -0700

    Constrain customer form

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,5 @@
 class Customer < ApplicationRecord
+  validates :name, :phone_num, :order_item_id, presence: true
+  validates :phone_num, :numericality => {:only_integer => true}
   belongs_to :order_item
 end
